4c7a001ce2b41bbaa31debd9bc689045de15581e
Parsa <hoseininejad1999@gmail.com>
2026-01-13 18:42:02 -0800

    Plot FastTrack data

--- plot.py
+++ plot.py
@@ -81,3 +81,47 @@ if __name__ == "__main__":
             relative_path = os.path.relpath(data_file_path, SFT_dir)
             plot_file_path = os.path.join(SFT_plot_dir, filename[:-4] + '.png')
             plot(data_file_path, plot_file_path)
+
+    data_dir = stats_dir + '/Tracking/data'
+    PE_dir = stats_dir + '/Tracking/data/PoseEstimationKernel'
+    SLP_dir = stats_dir + '/Tracking/data/SearchLocalPointsKernel'
+    SM_dir = stats_dir + '/Tracking/data/StereoMatchKernel'
+
+    plot_dir = stats_dir + '/Tracking/plots'
+    PE_plot_dir = stats_dir + '/Tracking/plots/PoseEstimationKernel/'
+    SLP_plot_dir = stats_dir + '/Tracking/plots/SearchLocalPointsKernel/'
+    SM_plot_dir = stats_dir + '/Tracking/plots/StereoMatchKernel/'
+    os.makedirs(plot_dir, exist_ok=True)
+    os.makedirs(PE_plot_dir, exist_ok=True)
+    os.makedirs(SLP_plot_dir, exist_ok=True)
+    os.makedirs(SM_plot_dir, exist_ok=True)
+
+    for filename in os.listdir(data_dir):
+        if filename.endswith('.txt'):
+            if (filename == 'num_frames_lost.txt'): continue
+            if (filename == 'kernel_initialization_time.txt'): continue
+            data_file_path = os.path.join(data_dir, filename)
+            relative_path = os.path.relpath(data_file_path, data_dir)
+            plot_file_path = os.path.join(plot_dir, filename[:-4] + '.png')
+            plot(data_file_path, plot_file_path)
+
+    for filename in os.listdir(PE_dir):
+        if filename.endswith('.txt'):
+            data_file_path = os.path.join(PE_dir, filename)
+            relative_path = os.path.relpath(data_file_path, PE_dir)
+            plot_file_path = os.path.join(PE_plot_dir, filename[:-4] + '.png')
+            plot(data_file_path, plot_file_path)
+
+    for filename in os.listdir(SLP_dir):
+        if filename.endswith('.txt'):
+            data_file_path = os.path.join(SLP_dir, filename)
+            relative_path = os.path.relpath(data_file_path, SLP_dir)
+            plot_file_path = os.path.join(SLP_plot_dir, filename[:-4] + '.png')
+            plot(data_file_path, plot_file_path)
+
+    for filename in os.listdir(SM_dir):
+        if filename.endswith('.txt'):
+            data_file_path = os.path.join(SM_dir, filename)
+            relative_path = os.path.relpath(data_file_path, SM_dir)
+            plot_file_path = os.path.join(SM_plot_dir, filename[:-4] + '.png')
+            plot(data_file_path, plot_file_path)
